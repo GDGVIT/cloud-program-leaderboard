e6728a8b44298e36d5bdf5439bd5c923939a6dee
Sandeep <rayanuthalas@gmail.com>
2020-10-05 14:41:01 +0530

    Added Docker Support

--- backend/settings.py
+++ backend/settings.py
@@ -1,6 +1,7 @@
 import os
 from decouple import config
 import django_heroku
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -61,6 +62,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config()
 
 AUTH_PASSWORD_VALIDATORS = [
     {
